13882d43644e59834593009ad1db659c21537ca5
factualmayur <factualmayur@gmail.com>
2024-12-04 11:54:54 +0530

    transition added

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -4,7 +4,7 @@ function SearchResults({ data }) {
   return (
     <section className="w-full overflow-y-auto mt-44 no-scrollbar">
       <div className="flex items-center justify-center w-full h-full">
-        <div className="flex flex-wrap justify-center w-full max-w-screen-xl gap-6 p-4 ">
+        <div className="flex flex-wrap justify-center w-full max-w-screen-xl gap-6 p-4 transition-all duration-300 ">
           {data?.map(({ name, image, text, price }) => (
             <div className="flex items-center w-full overflow-hidden rounded-lg shadow-lg bg-neutral-900 sm:w-80 md:w-96">
               <div className="w-full">
